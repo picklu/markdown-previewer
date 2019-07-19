31d1dc46e599dca6a83000502ba95ee63dfa620b
Subrata Sarker <subrata_sarker@yahoo.com>
2019-07-19 22:53:20 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Markdown Previewer
+
+The app is available at https://codepen.io/picklu/full/GVgGoo
+This project was built for freeCodeCamp
+
+This project was bootstrapped with React, Babel-Standalone, and Material-Ui.
---
+++ js/app.js
@@ -0,0 +1,110 @@
+/***************************************
+ *
+ * Material-Ui styles
+ *
+ * *************************************/
+const {
+  colors,
+  CssBaseline,
+  MuiThemeProvider,
+  Typography,
+  Container,
+  makeStyles,
+  createMuiTheme,
+  Box,
+  Button,
+  Icon,
+  Link,
+  CircularProgress
+} = MaterialUI;
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#556cd6'
+    },
+    secondary: {
+      main: '#19857b'
+    },
+    tertiary: {
+      main: 'ff2255'
+    },
+    error: {
+      main: colors.red.A400
+    },
+    background: {
+      default: '#556cd6'
+    }
+  }
+});
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: 'white',
+    borderRadius: '5px',
+    fontSize: '1.5rem',
+    height: '500px',
+    marginTop: '100px',
+    display: 'flex',
+    padding: '0'
+  },
+  container: {
+    border: '1px solid red',
+    fontSize: 'inherit',
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  }
+}));
+
+/***************************************
+ *
+ * Javascript
+ *
+ * *************************************/
+
+//  Concatenation of classes
+const clsx = (...classes) => {
+  return Array.from(classes).join(' ');
+};
+
+// Footer
+const Footer = () => {
+  const classes = useStyles();
+
+  return (
+    <Box textAlign='center' color='white' margin={2}>
+      <Typography>&copy; 2019 picklu</Typography>
+    </Box>
+  );
+};
+
+// Markdown Previewer
+const MarkdownPreviewer = () => {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <Container maxWidth='lg' className={classes.root}>
+        <Box className={classes.container}>Editor</Box>
+        <Box className={classes.container}>Previewer</Box>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+// Main app
+const App = () => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <MarkdownPreviewer />
+      <Footer />
+    </MuiThemeProvider>
+  );
+};
+
+// Mouting app to the DOM
+ReactDOM.render(<App />, document.getElementById('app'));
